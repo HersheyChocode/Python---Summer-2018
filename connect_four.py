20eb94c386063e6f4d59410976545b1a2aaf3137
Hrishikesh <Hrishikesh@hrishikeshs-mbp.lan>
2018-09-29 14:33:16 -0700

    This is the final version of the code for connect 4 game in python

--- connect_four.py
+++ connect_four.py
@@ -10,10 +10,11 @@ def make_gameBoard(board): #Function to create a game board
         gameBoard = nums+gameBoard #Adds the numbers on top of the game board
         print(gameBoard) #prints gameboard
         return(gameBoard)#returns gameboard
+    
 def turnX(board):
-        xColumn= int(input(playerX+" you're X. What column do you want to play in?")) # Input of wanted column
-        while xColumn>6 or xColumn<0:
-                xColumn = int(input("Please enter a valid answer: "))
+        xColumn= int(input(playerX+" you are X. What column do you want to play in?")) # Input of wanted column
+        while xColumn>6 or xColumn<0: #While the input is out of range
+                xColumn = int(input("Please enter a valid answer: ")) #New input
         for row in range(5,-1,-1): #For loop for row from 5 to 0
                 if board[row][xColumn]=='. ': #If the row'th row and the wanted column is a dot...
                         board[row][xColumn]='X ' # change it to an X
@@ -22,10 +23,11 @@ def turnX(board):
                         board[row][xColumn]='\nX ' #Make it a new line X
                         return board #Return this board
         return 'none' #If all are taken up, return none
+    
 def turnO(board):
-        oColumn= int(input(playerO+" you're O. What column do you want to play in?")) # Input of wanted column
-        while oColumn>6 or oColumn<0:
-                oColumn = int(input("Please enter a valid answer: "))
+        oColumn= int(input(playerO+" you are O. What column do you want to play in?")) # Input of wanted column
+        while oColumn>6 or oColumn<0: #While the input is out of range
+                oColumn = int(input("Please enter a valid answer: ")) #New input
         for row in range(5,-1,-1): #For loop for row from 5 to 0
                 if board[row][oColumn]=='. ': #If the row'th row and the wanted column is a dot...
                         board[row][oColumn]='O ' #change it to an O
@@ -34,6 +36,7 @@ def turnO(board):
                         board[row][oColumn]='\nO ' #Make it a new line O
                         return board #Return this board
         return 'none' #If all are taken up, return none
+    
 def vertical_possibilities(board): #Function for checking if a vertical line won
         for char in range(7): #for loop for six times
                 check = '' # Empty string for the character of each column
@@ -44,6 +47,7 @@ def vertical_possibilities(board): #Function for checking if a vertical line won
                 elif 'O O O O' in check: #If the list has 4 O's in a row
                     return 'O' #Return O
         return 'none' #If nothing is possible, return none
+    
 def horizontal_possibilities(board): #Function for checking if a horizontal line won
         for row in board: # For the row in board
                 check = '' #Creates empty string
@@ -54,6 +58,7 @@ def horizontal_possibilities(board): #Function for checking if a horizontal line
                 elif 'O O O O' in check: #If there are 4 O's in a row
                         return 'O' #Return O
         return 'none'# If nothing is possible, return none
+    
 def left_diagonal_possibilities(board): #Function for checking the left diagonal possibilities
         for row in range(3): #For loop in range 3
                 for column in range(3,7): #For loop in range 3 to 6
@@ -65,6 +70,7 @@ def left_diagonal_possibilities(board): #Function for checking the left diagonal
                         elif 'O O O O' in check: #If there are 4 O's in a row
                                 return 'O' #Return O
         return 'none' #If nothing is possible, return none
+    
 def right_diagonal_possibilities(board): #Function for checking the right diagonal possibilities
         for row in range(3): #For loop in range 3
                 for column in range(4): #For loop in range 4
@@ -76,37 +82,53 @@ def right_diagonal_possibilities(board): #Function for checking the right diagon
                         elif 'O O O O' in check: #If there are 4 O's in a row
                                 return 'O' #Return O
         return 'none' #If nothing is possible, return none
+    
 rowDots0 = ['\n. ','. ','. ','. ','. ','. ','. '] #First row of dots
 rowDots1 = ['\n. ','. ','. ','. ','. ','. ','. '] #Second row of dots
 rowDots2 = ['\n. ','. ','. ','. ','. ','. ','. '] #Third row of dots
 rowDots3 = ['\n. ','. ','. ','. ','. ','. ','. '] #Fourth row of dots
 rowDots4 = ['\n. ','. ','. ','. ','. ','. ','. '] #Fifth row of dots
 rowDots5 = ['\n. ','. ','. ','. ','. ','. ','. '] #Sixth row of dots
+
+#Connect Four Game
+
 board = [rowDots0,rowDots1,rowDots2,rowDots3,rowDots4,rowDots5] #Creates the board with all rows of dots
 gameBoard = make_gameBoard(board) #Makes the gameboard with the board
+
 while True: #Repeats until broken
-        if '. ' not in gameBoard:
-            print("Wow! You guys Tied! Congratulations, and good game!")
-            break
+    
+        if '. ' not in gameBoard: #If there are no more possibilities
+            print("Wow! You guys Tied! Congratulations, and good game!") #Print tie statement
+            break #break
+            
         board = turnX(board) #Converts player X's turn into a board
+        
         if board == 'none': #If the columns has no more space
             print("It seems that this column is taken up; we will hand this turn to", playerO) #Swith to player O's turn
+            
         gameBoard = make_gameBoard(board) #Makes the gameboard from player X's turn
+        
         rd = right_diagonal_possibilities(board) #Checks for right diagonal possibilities
         ld =left_diagonal_possibilities(board) #Checks for left diagonal possibilities
         v = vertical_possibilities(board) #Checks for vertical possibilities
         h = horizontal_possibilities(board) #Checks for horizontal possibilities
+        
         if v=='X' or h=='X' or ld=='X' or rd=='X': #If either one of the possibilities has X winning
                 print(playerX, ', you won!') #Print a winning statement
                 break #Break the while loop
+                
         board = turnO(board) #Converts player O's turn into a board
+        
         if board == 'none': #If the column has no more space
             print("It seems that this column is taken up; we will hand this turn to", playerX) #Switch to Player X's turn
+            
         gameBoard = make_gameBoard(board) #Makes the gameboard from player Y's turn
+        
         rd = right_diagonal_possibilities(board) #Checks for right diagonal possibilities
         ld =left_diagonal_possibilities(board) #Checks for left diagonal possibilities
         v = vertical_possibilities(board) #Checks for vertical possibilities
         h = horizontal_possibilities(board) #Checks for horizontal possibilities
+        
         if v=='O' or h=='O' or ld=='O' or rd=='O': #If either one of the possibilities has X winning
                 print(playerO, ', you won!') #Print a winning statement
                 break #Break the while loop
